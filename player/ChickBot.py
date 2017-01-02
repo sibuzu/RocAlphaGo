c75c9c3630c315c10ff9570c06c21be9b59d7092
simon <mingshing.su@gmail.com>
2017-01-02 11:33:14 +0800

    ChickBot add parameter

--- player/ChickBot.py
+++ player/ChickBot.py
@@ -1,24 +1,42 @@
 #!/usr/bin/env python
 
-from AlphaGo.ai import GreedyPolicyPlayer
-from AlphaGo.models.policy import CNNPolicy
-from interface.gtp_wrapper import run_gtp, ExtendedGtpEngine
-
-import keras
-import tensorflow as tf
-from keras.backend import tensorflow_backend as K
-
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-K.set_session(tf.Session(config=config))
+from __future__ import print_function
+import argparse
 
 MODEL = 'data/models/policy_cnn12.json'
 WEIGHTS = 'data/models/weights_cnn12.hdf5'
 
-model = CNNPolicy.load_model(MODEL)
-model.model.load_weights(WEIGHTS)
+def play(model, weight):
+    from AlphaGo.ai import GreedyPolicyPlayer
+    from AlphaGo.models.policy import CNNPolicy
+    from interface.gtp_wrapper import run_gtp, ExtendedGtpEngine
+    import keras
+    import tensorflow as tf
+    from keras.backend import tensorflow_backend as K
 
-# policy.model.summary()
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    K.set_session(tf.Session(config=config))
 
-player = GreedyPolicyPlayer(model)
-run_gtp(player, name="ChickBot", version="0.1", helper_level=15, debug=True)
+    model = CNNPolicy.load_model(MODEL)
+    model.model.load_weights(WEIGHTS)
+
+    # policy.model.summary()
+
+    player = GreedyPolicyPlayer(model)
+    run_gtp(player, name="ChickBot", version="0.1", helper_level=15, debug=True)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run ChickBot Go Bot')
+    parser.add_argument('--model', default=MODEL,
+                        help='DL network model')
+    parser.add_argument('--weights', default=WEIGHTS,
+                        help='DL network weights')
+
+    args = parser.parse_args()
+
+    print("MODEL: {}".format(args.model))
+    print("WEIGHTS: {}".format(args.weights))
+    play(args.model, args.weights)
+
